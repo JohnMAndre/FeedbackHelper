3da01435df4ec6a40482bbc96e09eeb61028b72d
John <John@JohnMAndre.com>
2021-01-22 10:35:08 +0700

    Adding license text

--- FeedbackHelper/Ribbon.cs
+++ FeedbackHelper/Ribbon.cs
@@ -1,10 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
+﻿//Copyright 2017-2021 John M Andre (John At JohnMAndre dot COM)
+
+//This file is part of Feedback Helper.
+
+//Feedback Helper is free software: you can redistribute it And/Or modify
+//it under the terms Of the GNU General Public License As published by
+//the Free Software Foundation, either version 3 Of the License, Or
+//(at your option) any later version.
+
+//Feedback Helper is distributed In the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY Or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License For more details.
+
+//You should have received a copy Of the GNU General Public License
+//along with Feedback Helper.  If Not, see < https: //www.gnu.org/licenses/>.
+
+using System; 
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Text;
 using Office = Microsoft.Office.Core;
 
 // TODO:  Follow these steps to enable the Ribbon (XML) item:
--- FeedbackHelper/Ribbon1.cs
+++ FeedbackHelper/Ribbon1.cs
@@ -1,7 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿//Copyright 2017-2021 John M Andre (John At JohnMAndre dot COM)
+
+//This file is part of Feedback Helper.
+
+//Feedback Helper is free software: you can redistribute it And/Or modify
+//it under the terms Of the GNU General Public License As published by
+//the Free Software Foundation, either version 3 Of the License, Or
+//(at your option) any later version.
+
+//Feedback Helper is distributed In the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY Or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License For more details.
+
+//You should have received a copy Of the GNU General Public License
+//along with Feedback Helper.  If Not, see < https: //www.gnu.org/licenses/>.
+
 using Microsoft.Office.Tools.Ribbon;
 using Microsoft.Office.Interop.Word;
 using System.Xml;
